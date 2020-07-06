561c0477c97acbc867e3fbf01b81b4dea879f1fe
Pradeep <pradeeprocks1999@gmail.com>
2020-07-06 10:27:09 +0530

    just 2nd one

--- file2.js
+++ file2.js
@@ -2,3 +2,4 @@ console.log("Learning basics of git");
 console.log("use git status to see status");
 console.log("use git log to see history");
 console.log("git for fun");
+console.log("git 2nd change");
